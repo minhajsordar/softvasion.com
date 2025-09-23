0e9dc8dac82fd9a10083a5efdc183893a7f38e59
minhajsordar <minhajsordar@gmail.com>
2025-09-23 21:34:56 +0600

    update menu

--- app/privecy-and-Policy/page.jsx
+++ app/privecy-and-Policy/page.jsx
@@ -74,7 +74,7 @@ export default function Page() {
 
 export function  generateMetadata(){
     return{
-        title:"Softvasion | Privecy-Policy",
+        title:"SoftVasion | Privecy-Policy",
         description:"Softvasion is an IT service provider in Bangladesh. We have all types of IT services. We also have 24 hour customer care service."
     }
 }
